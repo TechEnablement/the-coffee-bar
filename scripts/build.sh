4543e558b32a363d36d08131033335fecad08168
Goose1451 <wgodard@gmail.com>
2021-09-15 14:44:01 -0700

    lab0 test

---
+++ scripts/build.sh
@@ -0,0 +1,4 @@
+docker build -t sumo/the-coffee-bar-app:js-apps applications/the-coffee-bar-frontend/
+docker build -t sumo/the-coffee-bar-app:ruby-apps applications/ruby-the-coffee-bar-apps/
+docker build -t sumo/the-coffee-bar-app:python-apps applications/python-the-coffee-bar-apps/
+docker build -t sumo/the-coffee-bar-app:calculator-dotnet applications/dotnet-core-the-coffee-bar-app/
